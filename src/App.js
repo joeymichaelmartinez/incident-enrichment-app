fcfbcc279d52eaf310e88afe4ae585f7e810e7b7
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2021-01-23 16:40:06 -0800

    Commiting example with inputs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EnrichedMap from './EnrichedMap.js';
+import WeatherTable from './WeatherTable.js'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+  const [area, setArea] = useState("No Area Selected");
+  const [mapCenter, setMapCenter] = useState({
+    lat: 0,
+    lng: 0
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(e.target[0].value);
+    setMapCenter({
+      lat: parseInt(e.target[0].value),
+      lng: parseInt(e.target[1].value)
+    })
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'x-api-key': '5jXgI7SclB3gf4QB2dmP1ra6RQRqRVtH' },
+      // body: JSON.stringify({ title: 'React POST Request Example' })
+  };
+  fetch('https://api.meteostat.net/v2/stations/nearby?lat=' + e.target[0].value + '&lon=' + e.target[1].value + '&limit=3', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.data[0]);
+        setArea(data.data[0].name.en);
+        fetch('https://api.meteostat.net/v2/stations/hourly?station=' + data.data[0].id + '&start=2020-02-01&end=2020-02-04' , requestOptions)
+          .then(response => response.json())
+          .then(data => {
+          console.log(data.data[0]);
+          setArea(data.data[0].name.en);
+            
+          });
+      });
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Row>
+          <Col xs={6}>
+            <EnrichedMap center={mapCenter}/>
+          </Col>
+          <Col xs={6}>
+          <h1> {area} </h1>
+            <WeatherTable />
+          </Col>
+        </Row>
+        <Row>
+          <form onSubmit={(e) => handleSubmit(e)}>        
+            <label>
+              Latitude:
+            <input type="text" name="latitude" />        
+            </label>
+            <label>
+              Longitude:
+            <input type="text" name="longitude" />        
+            </label>
+            <label>
+              Start of incident:
+            <input type="text" name="latitude" />        
+            </label>
+            <label>
+              End of incident:
+            <input type="text" name="longitude" />        
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </Row>
     </div>
   );
 }
---
+++ src/EnrichedMap.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '100%'
+};
+
+
+function MyComponent(props) {
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDi38KdFAbNoPNz38LrUlZUQbLw2vIVV9A"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={props.center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(MyComponent)
